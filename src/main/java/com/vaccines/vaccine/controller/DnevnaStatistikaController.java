2b726f41e18a249c1162849aae1b217d1def7429
xods <obradovic.rade07@gmail.com>
2022-12-29 21:00:27 +0100

    U kontroler za statistiku dodate funkcionalnosti za dobavljanje pojedinacnog posta i za objavljivanje jednog posta.

--- src/main/java/com/vaccines/vaccine/controller/DnevnaStatistikaController.java
+++ src/main/java/com/vaccines/vaccine/controller/DnevnaStatistikaController.java
@@ -1,9 +1,49 @@
 package com.vaccines.vaccine.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vaccines.vaccine.dto.DnevnaStatistikaDTO;
+import com.vaccines.vaccine.entity.DnevnaStatistika;
+import com.vaccines.vaccine.repository.DnevnaStatistikaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
-@RequestMapping("dnevnaStatistika")
+@RequestMapping("statistika")
 public class DnevnaStatistikaController {
+
+    @Autowired
+    DnevnaStatistikaRepository statistikaRepository;
+
+    @GetMapping
+    public ResponseEntity<DnevnaStatistikaDTO> getDanas(){
+        DnevnaStatistika statistika = statistikaRepository.findByVremeObjavljivanja(new Date());
+
+        if(statistika != null){
+            return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new DnevnaStatistikaDTO(), HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<DnevnaStatistikaDTO> post(@RequestBody DnevnaStatistikaDTO statistikaDTO){
+        DnevnaStatistika statistika = statistikaRepository.findByVremeObjavljivanja(new Date());
+
+        if(statistika != null){
+            return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.SEE_OTHER);
+        }
+
+        statistika = new DnevnaStatistika();
+        statistika.setOboleli24(statistikaDTO.getOboleli24());
+        statistika.setTestirani24(statistikaDTO.getTestirani24());
+        statistika.setUkupnoOboleli(statistikaDTO.getUkupnoOboleli());
+        statistika.setHospitalizovani(statistikaDTO.getHospitalizovani());
+        statistika.setNaRespiratoru(statistikaDTO.getNaRespiratoru());
+        statistika.setVremeObjavljivanja(new Date());
+
+        return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.CREATED);
+    }
 }
