6e726367dadc1e675fc933ef1baf36ee065c963d
xods <obradovic.rade07@gmail.com>
2022-12-29 20:59:03 +0100

    Izmenjen Repository za dnevnu statistiku.

--- src/main/java/com/vaccines/vaccine/repository/DnevnaStatistikaRepository.java
+++ src/main/java/com/vaccines/vaccine/repository/DnevnaStatistikaRepository.java
@@ -2,9 +2,14 @@ package com.vaccines.vaccine.repository;
 
 import com.vaccines.vaccine.entity.DnevnaStatistika;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository("dnevnaStatistika")
 public interface DnevnaStatistikaRepository extends JpaRepository<DnevnaStatistika, Long> {
+    DnevnaStatistika findByVremeObjavljivanja(@Nullable Date vremeObjavljivanja);
+
 
 }
