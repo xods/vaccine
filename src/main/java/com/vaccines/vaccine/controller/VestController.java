b34a7add146c01dd571a7ee35755f39cb2c578e1
xods <obradovic.rade07@gmail.com>
2022-12-29 21:01:23 +0100

    U controlleru za vesti dodate funkcionalnosti za dobavljanje svih vesti i za objavu pojedinacne.

--- src/main/java/com/vaccines/vaccine/controller/VestController.java
+++ src/main/java/com/vaccines/vaccine/controller/VestController.java
@@ -1,9 +1,46 @@
 package com.vaccines.vaccine.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vaccines.vaccine.dto.VakcinaDTO;
+import com.vaccines.vaccine.dto.VestDTO;
+import com.vaccines.vaccine.entity.Vest;
+import com.vaccines.vaccine.repository.VestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("vest")
 public class VestController {
+
+    @Autowired
+    VestRepository vestRepository;
+
+    @GetMapping
+    public ResponseEntity<List<VestDTO>> getAll(){
+        List<Vest> vesti = vestRepository.findAll();
+
+        // Convert categories to DTOs
+        List<VestDTO> vestiDTO = new ArrayList<>();
+        for (Vest v : vesti) {
+            vestiDTO.add(new VestDTO(v));
+        }
+
+        return new ResponseEntity<>(vestiDTO, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<VestDTO> post(@RequestBody VestDTO vestDTO){
+        Vest vest = new Vest();
+
+        vest.setNaziv(vestDTO.getNaziv());
+        vest.setSadrzaj(vestDTO.getSadrzaj());
+        vest.setVremeObjavljivanja(new Date());
+
+        return new ResponseEntity<>(new VestDTO(vest), HttpStatus.CREATED);
+    }
 }
