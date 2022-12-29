8379d0b6e826cd83745d9f0c636e582f2d122a0a
xods <obradovic.rade07@gmail.com>
2022-12-29 20:01:55 +0100

    Promenjena baza sa H2 na MySQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (`email`, `password`,`uloga`) VALUES('rade@admin.com', '123rr321', 'ADMIN');
+INSERT INTO users (`email`, `password`,`uloga`) VALUES('rade@staff.com', '123rr321', 'STAFF');
+INSERT INTO users (`email`, `password`,`uloga`) VALUES('rade@pac.com', '123rr321', 'PATIENTS');
