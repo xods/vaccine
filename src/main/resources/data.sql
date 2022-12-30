d4dbb93fa2437c27728317296c1f2411055eea48
xods <obradovic.rade07@gmail.com>
2022-12-30 19:12:30 +0100

    U data.sql dodati redovi za vakcine i proizvođace.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,14 @@ INSERT INTO dnevne_statistike (hospitalizovani, na_respiratoru, oboleli_24, test
     VALUE (3241, 256, 7564, 20134, 17564, '2022-12-20T10:51');
 INSERT INTO dnevne_statistike (hospitalizovani, na_respiratoru, oboleli_24, testirani_24, ukupno_oboleli, vreme_objavljivanja)
     VALUE (3241, 256, 7564, 20134, 17564, '2022-12-21T10:51');
+
+INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (1, 'Srbija', 'SrboHem');
+INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (2, 'Kina', 'KinoHem');
+INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (3, 'Nemacka', 'SvaboHem');
+
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (30, 'SrboBoc', 1);
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (32, 'SrboKrk', 1);
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (23, 'KinoBoc', 2);
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (45, 'KinoĆap', 2);
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (56, 'SvaboBoc', 3);
+INSERT INTO vakcine(kolicina, naziv, proizvodjac_id) VALUE (56, 'SvaboAk', 3);
