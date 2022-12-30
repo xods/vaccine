c5bfdb886801c4b32f52c4468982addc8f23d097
xods <obradovic.rade07@gmail.com>
2022-12-30 19:14:30 +0100

    U repozitorijumu dodata funkcija za pretragu.

--- src/main/java/com/vaccines/vaccine/repository/VakcinaRepository.java
+++ src/main/java/com/vaccines/vaccine/repository/VakcinaRepository.java
@@ -1,9 +1,14 @@
 package com.vaccines.vaccine.repository;
 
 import com.vaccines.vaccine.entity.Vakcina;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("vakcina")
 public interface VakcinaRepository extends JpaRepository<Vakcina, Long> {
+    List<Vakcina> findByNazivContainsIgnoreCaseOrProizvodjac_NazivContainsIgnoreCaseOrProizvodjac_DrzavaContainsIgnoreCase(@Nullable String naziv, @Nullable String naziv1, @Nullable String drzava, Sort sort);
 }
