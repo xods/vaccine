4dac19bb4c6344ac4fcb54101a6248021454fba8
xods <obradovic.rade07@gmail.com>
2023-01-28 15:25:59 +0100

    Izmenjen fajlovi za dnevnu statistiku.

--- src/main/java/com/vaccines/vaccine/controller/DnevnaStatistikaController.java
+++ src/main/java/com/vaccines/vaccine/controller/DnevnaStatistikaController.java
@@ -3,6 +3,7 @@ package com.vaccines.vaccine.controller;
 import com.vaccines.vaccine.dto.DnevnaStatistikaDTO;
 import com.vaccines.vaccine.entity.DnevnaStatistika;
 import com.vaccines.vaccine.repository.DnevnaStatistikaRepository;
+import com.vaccines.vaccine.service.DnevnaStatistikaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,11 @@ import java.util.Date;
 public class DnevnaStatistikaController {
 
     @Autowired
-    DnevnaStatistikaRepository statistikaRepository;
+    DnevnaStatistikaService service;
 
     @GetMapping
     public ResponseEntity<DnevnaStatistikaDTO> getDanas(){
-        DnevnaStatistika statistika = statistikaRepository.findByVremeObjavljivanja(new Date());
+        DnevnaStatistika statistika = service.findByVremeObjavljivanja(new Date());
 
         if(statistika != null){
             return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.OK);
@@ -30,7 +31,7 @@ public class DnevnaStatistikaController {
 
     @PostMapping(consumes = "application/json")
     public ResponseEntity<DnevnaStatistikaDTO> post(@RequestBody DnevnaStatistikaDTO statistikaDTO){
-        DnevnaStatistika statistika = statistikaRepository.findByVremeObjavljivanja(new Date());
+        DnevnaStatistika statistika = service.findByVremeObjavljivanja(new Date());
 
         if(statistika != null){
             return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.SEE_OTHER);
@@ -44,7 +45,7 @@ public class DnevnaStatistikaController {
         statistika.setNaRespiratoru(statistikaDTO.getNaRespiratoru());
         statistika.setVremeObjavljivanja(new Date());
 
-        statistika = statistikaRepository.save(statistika);
+        statistika = service.save(statistika);
 
         return new ResponseEntity<>(new DnevnaStatistikaDTO(statistika), HttpStatus.CREATED);
     }
