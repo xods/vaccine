4f32afd03e38694d790f12181c7bd5f6a9ce86bd
xods <obradovic.rade07@gmail.com>
2023-03-11 16:05:29 +0100

    Dodata mogucnost dodavanja vesti.

--- src/main/java/com/vaccines/vaccine/controller/VestController.java
+++ src/main/java/com/vaccines/vaccine/controller/VestController.java
@@ -2,23 +2,43 @@ package com.vaccines.vaccine.controller;
 
 import com.vaccines.vaccine.dto.VakcinaDTO;
 import com.vaccines.vaccine.dto.VestDTO;
+import com.vaccines.vaccine.entity.ERole;
 import com.vaccines.vaccine.entity.Vest;
 import com.vaccines.vaccine.service.VestService;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.ServletContextAware;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/vest")
-public class VestController {
+public class VestController implements ServletContextAware {
 
     @Autowired
     VestService vestService;
+    @Autowired
+    ServletContext servletContext;
+    private  String bURL;
+
+    @PostConstruct
+    public void init() {
+        bURL = servletContext.getContextPath()+"/";
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
 
     @GetMapping
     public ResponseEntity<List<VestDTO>> getAll(){
@@ -33,16 +53,21 @@ public class VestController {
         return new ResponseEntity<>(vestiDTO, HttpStatus.OK);
     }
 
-    @PostMapping(consumes = "application/json")
-    public ResponseEntity<VestDTO> post(@RequestBody VestDTO vestDTO){
+    @PostMapping(value = "/")
+    public void post(@RequestParam String naziv,
+                     @RequestParam String sadrzaj,
+                     HttpSession session, HttpServletResponse response) throws IOException {
+        if(session.getAttribute("user") == null || session.getAttribute("role") == ERole.PATIENTS.toString()){
+            response.sendRedirect(bURL);
+        }
         Vest vest = new Vest();
 
-        vest.setNaziv(vestDTO.getNaziv());
-        vest.setSadrzaj(vestDTO.getSadrzaj());
+        vest.setNaziv(naziv);
+        vest.setSadrzaj(sadrzaj);
         vest.setVremeObjavljivanja(new Date());
 
-        vest = vestService.save(vest);
+        vestService.save(vest);
 
-        return new ResponseEntity<>(new VestDTO(vest), HttpStatus.CREATED);
+        response.sendRedirect(bURL);
     }
 }
