b7864860d4f25af37d2659c1202fad24969d9836
xods <obradovic.rade07@gmail.com>
2023-03-17 20:25:25 +0100

    Sredjeni podaci finis.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,6 @@ INSERT INTO vesti (naziv, sadrzaj, vreme_objavljivanja) VALUE ('Vakcine za sve',
 INSERT INTO vesti (naziv, sadrzaj, vreme_objavljivanja) VALUE ('Vakcine na izmaku', 'Nema dovoljno vakcina.', '2022-12-27T14:24');
 INSERT INTO vesti (naziv, sadrzaj, vreme_objavljivanja) VALUE ('Zalihe na izmaku', 'Strucnjaci predvidjaju kolaps.', '2022-12-20T10:51');
 
-INSERT INTO dnevne_statistike (hospitalizovani, na_respiratoru, oboleli_24, testirani_24, ukupno_oboleli, vreme_objavljivanja)
-    VALUE (3241, 256, 7564, 20134, 17564, '2022-12-20T10:51');
-INSERT INTO dnevne_statistike (hospitalizovani, na_respiratoru, oboleli_24, testirani_24, ukupno_oboleli, vreme_objavljivanja)
-    VALUE (3241, 256, 7564, 20134, 17564, '2022-12-21T10:51');
-
 INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (1, 'Srbija', 'SrboHem');
 INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (2, 'Kina', 'KinoHem');
 INSERT INTO proizvodjaci (proizvodjac_id, drzava, naziv) VALUE (3, 'Nemacka', 'SvaboHem');
